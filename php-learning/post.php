53aa21d9a75a8dbf95bcb4acb1bbccba5788c2d2
sKawashima <bysk.333@gmail.com>
2016-07-26 11:01:45 +0900

    post.phpの実装

---
+++ php-learning/post.php
@@ -0,0 +1,10 @@
+<?php
+require('set-db.php');
+
+if(mysqli_query($link,"INSERT INTO `Images` (`id`, `pass`, `data`, `creater`, `date`) VALUES (NULL, 'pass', 'data', 'creater', CURRENT_TIMESTAMP)")){
+	header("location: chk-db.php");
+	exit();
+}else{
+	echo "Failure";
+}
+
